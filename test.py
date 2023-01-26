26f9c5e1735c557fbf9447251e3c02ec7d24c80a
Rafael Thomaz <114960583+rafahthomaz14@users.noreply.github.com>
2023-01-26 08:40:56 -0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+etiqueta='caminho da pasta'
+
+for root,dirs,file in os.walk(etiqueta):
+
+for file in files:
+new_file_path = os.path.join(etiqueta,file)
+
+if '.pdf' in file:
+os.remove(new_file_path)
