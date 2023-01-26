8d80904d0633d1406db58a59eb7a4527e4340cf6
Rafael Thomaz <114960583+rafahthomaz14@users.noreply.github.com>
2023-01-26 08:44:25 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-etiqueta='caminho da pasta'
+       etiqueta= r"C:\Users\ADM\Desktop\etiquetas"
 
-for root,dirs,file in os.walk(etiqueta):
+        for root,dirs,file in os.walk(etiqueta):
 
-for file in files:
-new_file_path = os.path.join(etiqueta,file)
+            for file in files:
+                new_file_path = os.path.join(etiqueta,file)
 
-if '.pdf' in file:
-os.remove(new_file_path)
+        if '.pdf' in file:
+            os.remove(new_file_path)
+
